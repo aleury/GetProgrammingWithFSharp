cbf89eca578d5d186feb0742ea1e478cafae07fb
Adam Eury <adam@coherentpath.com>
2020-11-08 20:26:37 -0500

    Simplify capstone 2

--- Capstone2/Program.fs
+++ Capstone2/Program.fs
@@ -20,25 +20,20 @@ let main (_argv: string []): int =
 
     let customer = { Name = name }
     let mutable account = Operations.openAccount balance customer
-    
-    let validOps = List.ofArray([|"deposit"; "withdraw"|])
 
     while true do
+        Console.WriteLine(sprintf "Your account balance is now $%M!" account.Balance)
+        
         Console.Write "What would you like to do? (deposit or withdraw) "
         let operation = Console.ReadLine()
         
-        if List.contains operation validOps then
-            Console.Write(sprintf "What amount would you like to %s? " operation)
-            let amount = Decimal.Parse(Console.ReadLine())
-            
-            Console.WriteLine(sprintf "Performing `%s` of %M from your account.." operation amount)
-            match operation with
-            | "deposit" -> account <- depositWithAudit amount account
-            | "withdraw" -> account <- withdrawWithAudit amount account
-            | _ -> account <- account // TODO: Remove this eventually.
-
-            Console.WriteLine(sprintf "Your account balance is now $%M!" account.Balance)
-        else
-            Console.WriteLine "I don't recognize that operation. Please try again..."
-
+        if operation = "exit" then Environment.Exit 0
+        
+        Console.Write(sprintf "What amount would you like to %s? " operation)
+        let amount = Decimal.Parse(Console.ReadLine())
+    
+        account <-
+            if operation = "deposit" then depositWithAudit amount account
+            elif operation = "withdraw" then withdrawWithAudit amount account
+            else account
     0
