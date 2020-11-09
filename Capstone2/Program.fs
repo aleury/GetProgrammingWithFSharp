7512a498b441d617df2210f9c654a88c814e6a3a
Adam Eury <adam@coherentpath.com>
2020-11-08 20:32:31 -0500

    Refactor capstone

--- Capstone2/Program.fs
+++ Capstone2/Program.fs
@@ -12,26 +12,29 @@ let withdrawWithAudit =
 
 [<EntryPoint>]
 let main (_argv: string []): int =
-    Console.Write "Enter your name: "
-    let name = Console.ReadLine()
+    let name =
+        Console.Write "Enter your name: "
+        Console.ReadLine()
 
-    Console.Write "Enter your opening balance: "
-    let balance = Decimal.Parse(Console.ReadLine())
+    let balance =
+        Console.Write "Enter your opening balance: "
+        Console.ReadLine() |> Decimal.Parse
 
     let customer = { Name = name }
     let mutable account = Operations.openAccount balance customer
 
     while true do
-        Console.WriteLine(sprintf "Your account balance is now $%M!" account.Balance)
-        
-        Console.Write "What would you like to do? (deposit or withdraw) "
-        let operation = Console.ReadLine()
-        
+        let operation =
+            Console.WriteLine(sprintf "Your account balance is now $%M!" account.Balance)
+            Console.Write "What would you like to do? (deposit, withdraw, or exit) "
+            Console.ReadLine()
+
         if operation = "exit" then Environment.Exit 0
-        
-        Console.Write(sprintf "What amount would you like to %s? " operation)
-        let amount = Decimal.Parse(Console.ReadLine())
-    
+
+        let amount =
+            Console.Write(sprintf "What amount would you like to %s? " operation)
+            Console.ReadLine() |> Decimal.Parse
+
         account <-
             if operation = "deposit" then depositWithAudit amount account
             elif operation = "withdraw" then withdrawWithAudit amount account
